d430b9a6264adc640029f726c990c9d8fd8f211e
Amodu Temitope <amodut@gmail.com>
2015-12-10 13:17:52 +0100

    house cleaning in bucketlist and items models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,43 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  # before_action :this_is_current_user
-  # rescue_from NotAuthenticatedError do
-  #   render json: { error: 'Not Authorized' }, status: :unauthorized
-  # end
-  # rescue_from AuthenticationTimeoutError do
-  #   render json: { error: 'Auth token is expired' }, status: 419 # unofficial timeout status code
-  # end
 
   private
-
-  # Based on the user_id inside the token payload, find the user.
-  # def set_current_user
-  #   if decoded_auth_token
-  #     @current_user ||= User.find(decoded_auth_token[:user_id])
-  #   end
-  # end
-
-  # # Check to make sure the current user was set and the token is not expired
-  # def authenticate_request
-  #   if auth_token_expired?
-  #     render json: { error: 'Auth token is expired' }, status: 419 # unofficial timeout status code
-  #   elsif check_current_user_is_logged_in
-  #     render json: { error: 'You are not logged in' }, status: :unauthorized
-  #   else 
-  #     render json: { error: 'You are not Authorized'}, status: :unauthorized
-  #   end
-  # end
-
-  # def decoded_auth_token
-  #   @decoded_auth_token ||= AuthToken.decode(http_auth_header_content)
-  #   require 'pry'; binding.pry
-  # end
-
-  # def auth_token_expired?
-  #   decoded_auth_token && decoded_auth_token.expired?
-  # end
-
-
 
   def set_current_user
     authenticate_or_request_with_http_token do |token, option|  
@@ -47,21 +11,7 @@ class ApplicationController < ActionController::API
     end
   end
 
-
-
   def check_current_user_is_logged_in
     @current_user && @current_user.logged_in?
   end
-
-
- # def http_auth_header_content
- #    return @http_auth_header_content if defined? @http_auth_header_content
- #    @http_auth_header_content = begin
- #      if request.headers['Authorization'].present?
- #        request.headers['Authorization'].split(' ').last
- #      else
- #        nil
- #      end
- #    end
-  end
-# end
+end
