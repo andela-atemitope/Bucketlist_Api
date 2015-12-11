ab87a1b865cfcf04cceed8ae1abee57a3beae297
Amodu Temitope <amodut@gmail.com>
2015-12-11 14:38:29 +0100

    fixed error handling bug

--- app/controllers/auth_token.rb
+++ app/controllers/auth_token.rb
@@ -1,6 +1,7 @@
 require "jwt"
+require 'application_controller.rb'
 
-class AuthToken
+class AuthToken < ApplicationController
   def self.encode(payload, exp=24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
@@ -8,9 +9,9 @@ class AuthToken
 
   def self.decode(token)
     payload = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-    rescue JWT::ExpiredSignature
-      render json: { error: 'Token expired, Please generate a new one'} ; false 
-    rescue JWT::DecodeError
-      render json: { error: 'Invalid token' } ; false
+    # rescue JWT::ExpiredSignature
+    #   raise "Your token has expired"# @expired
+    # rescue JWT::DecodeError
+    #   raise "invalid token" # render json: { error: 'Invalid token' } ; false
   end
 end
